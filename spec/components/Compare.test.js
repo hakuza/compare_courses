d607cf343152942cb23694e57534c9b2e26c29c3
jsonbao <jsonbao@gmail.com>
2018-03-08 20:15:57 -0800

    Re-added testing files

--- spec/components/Compare.test.js
+++ spec/components/Compare.test.js
@@ -1,5 +1,9 @@
+import React from 'react';
+import ReactShallowRenderer from 'react-test-renderer';
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+// import Compare from '../../client/index.jsx';
+
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
 // Make Enzyme functions available in all test files without importing
@@ -10,3 +14,9 @@ global.mount = mount;
 test('test test', () => {
   expect(1 + 1).toEqual(2);
 });
+
+// test('should render Compare correctly', () => {
+//   const renderer = new ReactShallowRenderer();
+//   renderer.render(<Compare />);
+//   expect(renderer.getRenderOutput()).toMatchSnapshot();
+// });
